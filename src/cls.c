a181176ee4f23e403ba793733ee9268856de881b
N.V.S.Rahul <146009913+NVSRahul@users.noreply.github.com>
2026-01-20 22:24:06 +0530

    Clean up comments in cls.c

--- src/cls.c
+++ src/cls.c
@@ -1,13 +1,8 @@
 #include <unistd.h>
 
 int main(void) {
-	// The Magic Sequence:
-	// \x1b     -> The ESC (Escape) character
-	// [H       -> Move cursor to Home (top-left)
-	// [2J      -> Clear the entire visible screen
-	// [3J      -> Clear the scrollback buffer (the "true clean")
-	
-	// 11 is the exact length of the string inside the quotes
+
+	// The 11 bit Alias
 	write(1, "\x1b[H\x1b[2J\x1b[3J", 11);
 	return 0;
 }
